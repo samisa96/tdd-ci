14ddf217aa5cbb64564979cd6422ddb0d4e2a1c5
Yaniv Ben Zvi <45328424+samisa96@users.noreply.github.com>
2020-03-18 15:27:53 +0200

    src

---
+++ src/Jewishcal.py
@@ -0,0 +1,43 @@
+import requests
+
+
+class Jewishcal:
+    """
+    @ref: https://www.hebcal.com/home/195/jewish-calendar-rest-api
+    """
+
+    @staticmethod
+    def rest_times(month, year):
+       pass
+
+    @staticmethod
+    def specific_date_holiday(month, year):
+        pass
+
+    @staticmethod
+    def display_holidays(month, year):
+        pass
+
+    @staticmethod
+    def display_candles(month, year):
+       pass
+
+    @staticmethod
+    def display_parashat(month, year):
+        pass
+
+    @staticmethod
+    def convert_to_hebrew(year, month, day):
+        pass
+
+    @staticmethod
+    def print_convert():
+		pass
+    @staticmethod
+    def input_date(info):
+       pass
+
+
+if __name__ == "__main__":
+    Jewishcal.input_date()
+    Jewishcal.print_convert()
---
+++ src/main.py
@@ -0,0 +1,47 @@
+import os
+from time import sleep
+from src.Jewishcal import Jewishcal
+
+
+def clear():
+    sleep(3)
+    os.system('clear')
+
+
+def close():
+    clear()
+    print("program has been closed")
+    sleep(1)
+    exit(0)
+
+
+def info_request():
+    info = input('input "all" for all events,"h" for holidays, "c" for candles lighting and "p" for parashat')
+    return Jewishcal.input_date(info)
+
+
+def convert():
+    return Jewishcal.print_convert()
+
+
+def main():
+    while True:
+        print("""
+        1. Check jewish events in year/month
+        2. Convert Gregorian date to Jewish date
+        3. exit
+        """)
+
+        menu = input()
+        # clear()  # clear the screen
+
+        switcher = {
+            1: lambda x: info_request(),
+            2: lambda x: convert(),
+            3: lambda x: close(),
+        }
+        switcher.get(int(menu))(0)
+
+
+if __name__ == "__main__":
+    main()
